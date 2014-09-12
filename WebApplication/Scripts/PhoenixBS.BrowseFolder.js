fc0be722563b67de4810bf52cb023b5c0c938428
simonjschoch <simonjschoch@gmail.com>
2014-09-12 11:58:56 +0100

    UI WiP

--- WebApplication/Scripts/PhoenixBS.BrowseFolder.js
+++ WebApplication/Scripts/PhoenixBS.BrowseFolder.js
@@ -43,8 +43,6 @@ var WorkSiteFolders = function () {
     }
 
     var _loadSummary = function (index) {
-        //$("#main div.row").last();
-
         var path = "http://localhost/TestService/api/summary/5";
 
         $.ajax({
@@ -53,8 +51,6 @@ var WorkSiteFolders = function () {
             url: path,
             dataType: 'json',
             success: function (summary) {
-                var temp = $("#folder-template").html();
-
                 $("#folder-template").find("#id-" + index).text(summary.prj_id);
                 $("#folder-template").find("#name-" + index).text(summary.name);
                 $("#folder-template").find("#type-" + index).text(summary.type);
@@ -62,7 +58,29 @@ var WorkSiteFolders = function () {
                 $("#folder-template").find("#documents-" + index).text(summary.documents);
             }
         });
+    }
 
+    var _showDetails = function (id) {
+        var index = id.replace("btn-showdetails-", "");
+        var hidden = $("#main").find("#hdn-objectid-" + index);
+        var buttonPanel = hidden.parents("div.well").find("div:nth-child(2)");
+        var detailsPanel = hidden.parents("div.well").find("div:nth-child(3)");
+        detailsPanel.show();
+        detailsPanel.find("#tree-" + index).jstree();
+        var button = buttonPanel.find("#btn-showdetails-" + index);
+        button.text("Hide Details");
+        button.attr("onClick", "WorkSiteFolders.HideDetails(this.id); return false;");
+    }
+
+    var _hideDetails = function (id) {
+        var index = id.replace("btn-showdetails-", "");
+        var hidden = $("#main").find("#hdn-objectid-" + index);
+        var buttonPanel = hidden.parents("div.well").find("div:nth-child(2)");
+        var detailsPanel = hidden.parents("div.well").find("div:nth-child(3)");
+        detailsPanel.hide();
+        var button = buttonPanel.find("#btn-showdetails-" + index);
+        button.text("Show Details");
+        button.attr("onClick", "WorkSiteFolders.ShowDetails(this.id); return false;");
     }
 
     var _insert = function (folders) {
@@ -80,20 +98,20 @@ var WorkSiteFolders = function () {
                 if (!exists) {
                     var index = indexOld + 1;
 
-                    var temp = $("#folder-template").html();
-
                     $("#folder-template").find("#id-" + indexOld).attr("id", "id-" + index);
                     $("#folder-template").find("#name-" + indexOld).attr("id", "name-" + index);
                     $("#folder-template").find("#type-" + indexOld).attr("id", "type-" + index);
                     $("#folder-template").find("#subfolders-" + indexOld).attr("id", "subfolders-" + index);
                     $("#folder-template").find("#documents-" + indexOld).attr("id", "documents-" + index);
                     $("#folder-template").find("#hdn-objectid-" + indexOld).attr("id", "hdn-objectid-" + index);
+                    $("#folder-template").find("#btn-showdetails-" + indexOld).attr("id", "btn-showdetails-" + index);
                     $("#folder-template").find("#btn-remove-" + indexOld).attr("id", "btn-remove-" + index);
+                    $("#folder-template").find("#tree-" + indexOld).attr("id", "tree-" + index);
                     _loadSummary(index);
 
                     $("#folder-template").find("#hdn-objectid-" + index).attr("value", e);
-                    $("#folder-template").find("#btn-remove-" + index).attr("onClick", "WorkSiteFolders.Remove(this.id);");
-                    $("#folder-template").find("#name-" + index).text(e);
+                    $("#folder-template").find("#btn-showdetails-" + index).attr("onClick", "WorkSiteFolders.ShowDetails(this.id); return false;");
+                    $("#folder-template").find("#btn-remove-" + index).attr("onClick", "WorkSiteFolders.Remove(this.id); return false;");
                     return $("#folder-template").html();
                 }
             });
@@ -102,7 +120,7 @@ var WorkSiteFolders = function () {
 
     var _remove = function (id) {
         var index = id.replace("btn-remove-", "");
-        var panel = $("#main").find("#hdn-objectid-" + index).closest("div.row");
+        var panel = $("#main").find("#hdn-objectid-" + index).closest("div.container");
         panel.fadeOut("slow", function () {
             panel.remove();
         });
@@ -117,6 +135,14 @@ var WorkSiteFolders = function () {
         Remove: function (id) {
             _remove(id);
             _updatePostData();
+        },
+
+        ShowDetails: function (id) {
+            _showDetails(id);
+        },
+
+        HideDetails: function (id) {
+            _hideDetails(id);
         }
     }
 }();
